8e276a7058ab51005b414a35ff697137118cc176
Landon <hessland21@gmail.com>
2022-03-22 12:57:48 -0500

    make bot OOP

--- bot/main.py
+++ bot/main.py
@@ -1,26 +1,31 @@
 import json
 import discord
 
-client = discord.Client()
-token = None
 
-# Read discord bot token
-with open('./config.json', 'r') as file:
-	config = json.load(file)
-	token = config['token']
+def main():
+    token = None
 
-# Bot startup event
-@client.event
-async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+    # Read discord bot token
+    with open('./config.json', 'r') as file:
+        config = json.load(file)
+        token = config['token']
 
-# Message event
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-    if message.content == 'Hello':
-        await message.channel.send('Hello')
+    client = MyBot()
+    client.run(token)
+
+
+class MyBot(discord.Client):
+
+    # Bot startup event
+    async def on_ready(self):
+        print(f'We have logged in as {self.user}!')
+
+    # Message event
+    async def on_message(self, message):
+        if message.author == self.user:
+            return
+        if message.content == 'Hello':
+            await message.channel.send('Hello')
 
 if __name__ == '__main__':
-	client.run(token)
+    main()
